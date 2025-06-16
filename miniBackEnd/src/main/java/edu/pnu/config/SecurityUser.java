9a7583f37058444ef3befbc5e787e19f78715135
Hong jimin <hsk3232@pusan.ac.kr>
2025-06-16 10:06:14 +0900

    6/16

--- miniBackEnd/src/main/java/edu/pnu/config/SecurityUser.java
+++ miniBackEnd/src/main/java/edu/pnu/config/SecurityUser.java
@@ -10,7 +10,7 @@ public class SecurityUser extends User {
 	private static final long serialVersionUID = 1L;
 	
 	public SecurityUser(Member member) {
-		super(member.getId(), "{noop}" + member.getPassword(),
+		super(member.getUsername(), "{noop}" + member.getPassword(),
 				AuthorityUtils.createAuthorityList(member.getRole().toString()));
 	}
 	
